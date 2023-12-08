c145b7c2f899d1adf2a3bef07956d63a6ff8c964
M19604 <141754314+M19604@users.noreply.github.com>
2023-12-08 17:47:05 -0500

    test

--- index.js
+++ index.js
@@ -1,24 +1,74 @@
-const express = require("express");
-const { createServer } = require("node:http");
-const { join } = require("node:path");
-const { Server } = require("socket.io");
-const app = express();
-const server = createServer(app);
-const io = new Server(server, {
-    path: "/test/"
-});
-app.get("/", (req, res) => {
-    res.sendFile(join(__dirname, "index.html"));
-})
-io.on("connection", (socket) => {
-    console.log("User connected...");
-    socket.on("disconnect", () => {
-        console.log("User disconnected...");
-    })
-    socket.on("chat message", (msg) => {
-        io.emit("chat message", msg);
-    })
-})
-server.listen(3000, () => {
-    console.log("Port running on 3000...");
-})
+import React, { useEffect, useState } from "react";
+import io from "socket.io-client";
+
+let socket;
+
+const Home = () => {
+  const [message, setMessage] = useState("");
+  const [username, setUsername] = useState("");
+  const [allMessages, setAllMessages] = useState([]);
+
+  useEffect(() => {
+    socketInitializer();
+
+    return () => {
+      socket.disconnect();
+    };
+  }, []);
+
+  async function socketInitializer() {
+    await fetch("/api/socket");
+
+    socket = io();
+
+    socket.on("receive-message", (data) => {
+      setAllMessages((pre) => [...pre, data]);
+    });
+  }
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    console.log("emitted");
+
+    socket.emit("send-message", {
+      username,
+      message
+    });
+    setMessage("");
+  }
+
+  return (
+    <div>
+      <h1>Chat app</h1>
+      <h1>Enter a username</h1>
+
+      <input value={username} onChange={(e) => setUsername(e.target.value)} />
+
+      <br />
+      <br />
+
+      <div>
+        {allMessages.map(({ username, message }, index) => (
+          <div key={index}>
+            {username}: {message}
+          </div>
+        ))}
+
+        <br />
+
+        <form onSubmit={handleSubmit}>
+          <input
+            name="message"
+            placeholder="enter your message"
+            value={message}
+            onChange={(e) => setMessage(e.target.value)}
+            autoComplete={"off"}
+          />
+        </form>
+      </div>
+    </div>
+  );
+};
+
+export default Home;

92d374feae071258c73b1b58e39881e2400b2fd1
M19604 <141754314+M19604@users.noreply.github.com>
2023-12-08 16:36:33 -0500

    Update index.js

--- index.js
+++ index.js
@@ -4,7 +4,9 @@ const { join } = require("node:path");
 const { Server } = require("socket.io");
 const app = express();
 const server = createServer(app);
-const io = new Server(server);
+const io = new Server(server, {
+    path: "/test/"
+});
 app.get("/", (req, res) => {
     res.sendFile(join(__dirname, "index.html"));
 })
